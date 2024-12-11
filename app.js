d8415e3d1ccbd07a01d94431b62ae9b288df382c
V1ADDD <rusakvlad2@gmail.com>
2024-12-11 21:28:12 +0300

    refactor(basic-3.3): update css concise naming

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("undone");//ul of #undone
+var completedTasksHolder=document.getElementById("done");//done
 
 
 //New task list item
